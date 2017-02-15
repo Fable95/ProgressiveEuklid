c5d88ec27a506275388f6d12512aa5e9aeeef9d4
David Wittenbrink <david.wittenbrink@gmail.com>
2017-02-15 13:30:20 +0100

    Added html replacement into build chain

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ const gulp = require('gulp'),
       sourcemaps = require('gulp-sourcemaps'),
       concat = require('gulp-concat'),
       uglify = require('gulp-uglify'),
+      htmlreplace = require('gulp-html-replace'),
       babel = require('gulp-babel');
 
-gulp.task('default', () => {
+gulp.task("js-optimize", () => {
     return gulp.src('scripts/**/*.js')
         .pipe(sourcemaps.init())
         .pipe(babel({
@@ -14,5 +15,13 @@ gulp.task('default', () => {
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task("html-optimize", () => {
+    return gulp.src('index.html')
+        .pipe(htmlreplace({"compressed-js": 'scripts/app.js'}))
         .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', ['js-optimize', "html-optimize"]);
