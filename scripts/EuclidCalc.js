2b7471659a9eaf80b9f7d935ff5470bb456bfe86
Fabian Schmid <fabian.schmid@student.tugraz.at>
2017-02-15 19:14:39 +0100

    added vertical table support

--- scripts/EuclidCalc.js
+++ scripts/EuclidCalc.js
@@ -2,20 +2,20 @@
 
 class EuclidCalc{
 
-  static calcNextA(a, q, index){
+  static calcNextHorizontalA(a, q, index){
     return (a[index - 2] - a[index - 1] * q[index]);
   }
-  static calcNextB(b, q, index){
+  static calcNextHorizontalB(b, q, index){
     return (b[index - 2] - b[index - 1] * q[index]);
   }
-  static calcNextQ(r, index){
+  static calcNextHorizontalQ(r, index){
     return (Math.floor(r[index - 2] / r[index - 1]));
   }
-  static calcNextR(r, index){
+  static calcNextHorizontalR(r, index){
     return (r[index - 2] % r[index - 1]);
   }
   static calcHorizontal(x, y){
-    
+
     if(x < y){
       [x, y] = [y, x];
     }
@@ -29,10 +29,56 @@ class EuclidCalc{
 
     while(retObject.r[retObject.r.length - 1] != 0){
       let index = retObject.r.length;
-      retObject.r.push(this.calcNextR(retObject.r, index));
-      retObject.q.push(this.calcNextQ(retObject.r, index));
-      retObject.a.push(this.calcNextA(retObject.a, retObject.q, index));
-      retObject.b.push(this.calcNextB(retObject.b, retObject.q, index));
+      retObject.r.push(this.calcNextHorizontalR(retObject.r, index));
+      retObject.q.push(this.calcNextHorizontalQ(retObject.r, index));
+      retObject.a.push(this.calcNextHorizontalA(retObject.a, retObject.q, index));
+      retObject.b.push(this.calcNextHorizontalB(retObject.b, retObject.q, index));
+    }
+    return retObject;
+  }
+  static calcNextVerticalA(b, index){
+    return b[index - 1];
+  }
+  static calcNextVerticalB(a, b, index){
+    return (a[index - 1] % b[index - 1]);
+  }
+  static calcNextVerticalQ(a, b, index){
+    if(b[index] === 0){
+      return 0;
+    }
+    return (Math.floor(a[index] / b[index]));
+  }
+  static calcNextVerticalS(t, index){
+    return t[index + 1];
+  }
+  static calcNextVerticalT(s, t, q, index){
+    return (s[index + 1] - t[index + 1] * q[index]);
+  }
+  static calcVertical(a, b){
+    if(a < b){
+      [a, b] = [b, a];
+    }
+    let retObject = {
+      "a" : [a],
+      "b" : [b],
+      "q" : [],
+      "s" : [],
+      "t" : []
+    };
+    retObject.q.push(this.calcNextVerticalQ(retObject.a, retObject.b, 0));
+    while(retObject.b[retObject.b.length - 1] != 0){
+      let index = retObject.b.length;
+      retObject.a.push(this.calcNextVerticalA(retObject.b, index));
+      retObject.b.push(this.calcNextVerticalB(retObject.a, retObject.b, index));
+      retObject.q.push(this.calcNextVerticalQ(retObject.a, retObject.b, index));
+    }
+    let length = retObject.a.length;
+    retObject.s[length - 1] = 1;
+    retObject.t[length - 1] = 0;
+    for (let i = length - 1; i > 0; i--) {
+      retObject.s[i - 1] = this.calcNextVerticalS(retObject.t, i-1);
+      retObject.t[i - 1] = this.calcNextVerticalT(retObject.s, retObject.t, retObject.q, i-1);
+
     }
     return retObject;
   }
--- scripts/View.js
+++ scripts/View.js
@@ -3,10 +3,12 @@
 
 class View {
   constructor(){
+    this.input = document.querySelector('.input-wrapper');
+    this.viewSwitch = document.querySelector('.view-switch');
     this.inputX = document.querySelector('.x-input');
     this.inputY = document.querySelector('.y-input');
-    this.inputX.addEventListener("input", this.inputEventListener.bind(this));
-    this.inputY.addEventListener("input", this.inputEventListener.bind(this));
+    this.viewSwitch.addEventListener("click", this.inputEventListener.bind(this));
+    this.input.addEventListener("input", this.inputEventListener.bind(this));
   }
   static convertToTableRow(rowLabel, arr) {
     return `<tr><th>${rowLabel}</th>` +
@@ -18,14 +20,20 @@ class View {
   }
   inputEventListener(event){
     if(View.isInt(this.inputX.value) && View.isInt(this.inputY.value)){
-      View.generateHTMLTable(Number.parseInt(this.inputX.value), Number.parseInt(this.inputY.value));
+      let x = Number.parseInt(this.inputX.value);
+      let y = Number.parseInt(this.inputY.value);
+      if(this.viewSwitch.checked){
+        let tableData = EuclidCalc.calcVertical(x, y);
+        View.generateVerticalHTMLTable(tableData);
+      } else {
+        let tableData = EuclidCalc.calcHorizontal(x, y);
+        View.generateHorizontalHTMLTable(tableData);
+      }
     }
     console.log(this.inputX.value);
     console.log(this.inputY.value);
   }
-  static generateHTMLTable(x, y) {
-    let tableData = EuclidCalc.calcHorizontal(x, y);
-
+  static generateHorizontalHTMLTable(tableData) {
     let indexRow = "<tr><th>i</th>";
     for(let i = - 1; i < tableData.r.length - 1; i++){
       indexRow += `<th>${i}</th>`;
@@ -35,15 +43,27 @@ class View {
     let table = `
       <table>
         ${ indexRow }
-        ${ this.convertToTableRow("ai", tableData.a) }
-        ${ this.convertToTableRow("bi", tableData.b) }
-        ${ this.convertToTableRow("qi", tableData.q) }
-        ${ this.convertToTableRow("ri", tableData.r) }
+        ${ this.convertToTableRow("a<sub>i</sub>", tableData.a) }
+        ${ this.convertToTableRow("b<sub>i</sub>", tableData.b) }
+        ${ this.convertToTableRow("q<sub>i</sub>", tableData.q) }
+        ${ this.convertToTableRow("r<sub>i</sub>", tableData.r) }
       </table>
     `;
 
     document.querySelector('.euclid-table-wrapper').innerHTML = table;
-
+  }
+  static verticalTableRow(tableData, index){
+    let retArr = "";
+    return (`<tr><td>${tableData.a[index]}</td><td>${tableData.b[index]}</td><td>${tableData.q[index]}</td><td>${tableData.s[index]}</td><td>${tableData.t[index]}</td></tr>`);
+  }
+  static generateVerticalHTMLTable(tableData){
+    let headerRow = "<tr><th>a<sub>i</sub></th><th>b<sub>i</sub></th><th>q<sub>i</sub></th><th>s<sub>i</sub></th><th>t<sub>i</sub></th></tr>";
+    let table = `<table> ${headerRow}`;
+    for (let i = 0; i < tableData.a.length; i++) {
+      table += View.verticalTableRow(tableData, i);
+    }
+    table += "</table>";
+    document.querySelector('.euclid-table-wrapper').innerHTML = table;
   }
 }
 
